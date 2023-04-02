5b67f52c551b837ab56e66c661a9bfd1a6a793be
Aditya Narayanan <aditya.narayanan03@gmail.com>
2023-04-02 17:03:06 -0500

    Added a potentiometer example, seems to work decently

---
+++ arduino_library/potentiometer/potentiometer.ino
@@ -0,0 +1,27 @@
+#include <Plotter.h>
+
+#define BUFFER_SIZE 10
+
+Plotter plotter;
+
+int line_1;
+int buffer_x[BUFFER_SIZE];
+int buffer_y[BUFFER_SIZE];
+
+int pot_val;
+
+void setup(){
+    plotter.begin(19200);
+
+    line_1 = plotter.add_line(buffer_x, buffer_y, BUFFER_SIZE);
+}
+
+void loop(){
+    pot_val = analogRead(A0);
+
+    if (plotter.push_to_buffer(line_1, millis(), pot_val)){
+        plotter.send_buffer(line_1);
+    }
+
+    delay(1);
+}
